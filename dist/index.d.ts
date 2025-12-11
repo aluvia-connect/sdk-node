f84c55ce5d532f836829db9d97bd3447c5b0a196
Dennis Krol <denniskrol@gmail.com>
2025-12-11 09:55:05 +0800

    chore: Remove dist and node_modules from repo

--- dist/index.d.ts
+++
@@ -1,3 +0,0 @@
-export { AluviaClient } from './AluviaClient';
-export { MissingUserTokenError, InvalidUserTokenError, ApiError, ProxyStartError, } from './errors';
-export type { GatewayProtocol, LogLevel, AluviaClientOptions, AluviaClientSession, } from './types';
